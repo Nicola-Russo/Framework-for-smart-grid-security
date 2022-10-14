954b521276e41a81382b3e2b0e9c48fe5696c517
Nicola Russo <108270978+Nicola-Russo@users.noreply.github.com>
2022-10-14 10:13:15 +0200

    Add files via upload

---
+++ Data provenance platform based on blockchain tecnology/migrations/2_deploy_contracts.js
@@ -0,0 +1,46 @@
+const SpecificProvenance = artifacts.require("SpecificProvenance");
+
+module.exports = (deployer, network) => {
+  if (network === "live") {
+    return;
+  }
+
+  if (network === "development") {
+    console.log("Deploying contracts to Ganache");
+
+    deployer.then(async () => {
+      await deployer.deploy(SpecificProvenance);
+      const provenanceInstance = await SpecificProvenance.deployed();
+
+      console.log('\n*************************************************************************\n');
+      console.log(`Provenance Contract Address (Ganache): ${provenanceInstance.address}`);
+      console.log('\n*************************************************************************\n');
+    });
+  }
+
+  // Deploy on other network test
+  /*
+  if (network === "rinkeby") {
+    deployer.then(async () => {
+      await deployer.deploy(SpecificProvenance);
+      const provenanceInstance = await SpecificProvenance.deployed();
+
+      console.log('\n*************************************************************************\n');
+      console.log(`Provenance Contract Address (Rinkeby): ${provenanceInstance.address}`);
+      console.log('\n*************************************************************************\n');
+    });
+  }
+
+  if (network === "ropsten") {
+    deployer.then(async () => {
+      await deployer.deploy(SpecificProvenance);
+      const provenanceInstance = await SpecificProvenance.deployed();
+
+      console.log('\n*************************************************************************\n');
+      console.log(`Provenance Contract Address (Ropsten): ${provenanceInstance.address}`);
+      console.log('\n*************************************************************************\n');
+    });
+  }
+*/
+
+};
